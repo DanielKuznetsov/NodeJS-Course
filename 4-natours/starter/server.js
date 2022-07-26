17014bb472b4eab1431fd5f7934340823318e108
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-26 16:31:17 -0700

    creating new schema and a model for it

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -22,6 +22,26 @@ mongoose.connect(DB, { useUnifiedTopology: true }).then((connection) => {
 // console.log(app.get('env'));
 // console.log(process.env);
 
+// ! THIS IS BASIC SCHEMA
+const tourSchema = new mongoose.Schema({
+  name: {
+    // describing the type of data we want to pass in
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+// ! THIS IS BASIC MODEL TO THE SCHEMA RIGHT ABOVE
+const Tour = mongoose.Model('Tour', tourSchema); // use uppercase on names and variables in mongoose models
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App running on port ' + port);
