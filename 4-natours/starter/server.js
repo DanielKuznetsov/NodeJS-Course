4201e7336fd079920fdf6532d52ff593d64bca93
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-26 15:16:58 -0700

    connected mongodb database to our app

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -2,9 +2,22 @@
 // 3. START THE SERVER
 
 const dotenv = require('dotenv');
+const app = require(`./app.js`);
+
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
-const app = require(`./app.js`);
+// replacing password in the mongodb string with the password environment variable
+const DB = process.env.DATABASE.replace(
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
+);
+
+// code to connnect to the database
+mongoose.connect(DB, { useUnifiedTopology: true }).then((connection) => {
+  // console.log(connection.connections);
+  console.log('DB connection is established');
+});
 
 // console.log(app.get('env'));
 // console.log(process.env);
