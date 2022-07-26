37debcdce2c6ddfbc4c2c32e6d7df67c13b5c803
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-25 21:00:03 -0700

    serving static files

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Serving stati files
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello From the Middleware');
   next();
