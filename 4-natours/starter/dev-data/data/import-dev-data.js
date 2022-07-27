0c3d23e58832a8e3bece92fd272508e7c3346732
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-27 10:51:36 -0700

    wrote a sript to import or delete the data from the local json file

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+// Script that will import all of the dev data into the database
+
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, { useUnifiedTopology: true }).then((connection) => {
+  console.log('DB connection is established');
+});
+
+// READ JSON FILES
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+    process.exit();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+// DELETE DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(); // if passed nothing, it will delete all of the documents in the collection
+    console.log('Data successfully deleted');
+    process.exit();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// console.log(process.argv)
