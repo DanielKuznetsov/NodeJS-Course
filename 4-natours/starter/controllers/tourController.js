3fd81437b3b48836f36c2c4092a5e54a09ff9798
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-27 09:16:08 -0700

    delete a document

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -154,10 +154,24 @@ exports.updateTour = async (req, res) => {
   // });
 };
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err.message,
+    });
+  }
+
   // 204 means no content
-  res.status(204).json({
-    status: 'success',
-    data: 'null',
-  });
+  // res.status(204).json({
+  //   status: 'success',
+  //   data: 'null',
+  // });
 };
