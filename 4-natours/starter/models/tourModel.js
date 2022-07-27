54c13cb6a0e7dbd554e986c88334c505ef1d1de8
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-27 10:11:16 -0700

    updating schema

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -7,15 +7,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true, // trimming white space off the string
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5, // will be calculated later with real reviews
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0, // will be calculated later with real reviews
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // trimming white space off the string,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String, // reference to the file will be stored in the database
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String], // defining an array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 // ! THIS IS BASIC MODEL TO THE SCHEMA RIGHT ABOVE
 const Tour = mongoose.model('Tour', tourSchema); // use uppercase on names and variables in mongoose models
