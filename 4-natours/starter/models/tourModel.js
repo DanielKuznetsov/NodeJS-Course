d7db89bcb686973b9b3a2fd50828ca25318b5d5c
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-28 16:07:08 -0700

    document middleware and virtual properties

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,60 +1,85 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // ! THIS IS BASIC SCHEMA
-const tourSchema = new mongoose.Schema({
-  name: {
-    // describing the type of data we want to pass in
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true, // trimming white space off the string
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      // describing the type of data we want to pass in
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true, // trimming white space off the string
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5, // will be calculated later with real reviews
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0, // will be calculated later with real reviews
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, // trimming white space off the string,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String, // reference to the file will be stored in the database
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], // defining an array of strings
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // select: false, // hides form the API output
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5, // will be calculated later with real reviews
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0, // will be calculated later with real reviews
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true, // trimming white space off the string,
-    required: [true, 'A tour must have a summary'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String, // reference to the file will be stored in the database
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], // defining an array of strings
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // select: false, // hides form the API output
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
+
+// // DOCUMENT MIDDLEWARE: runds before .save() and .create()
+// tourSchema.pre('save', function (next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
+
+// // is executed after all pre middleware functions are executed
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
 // ! THIS IS BASIC MODEL TO THE SCHEMA RIGHT ABOVE
 const Tour = mongoose.model('Tour', tourSchema); // use uppercase on names and variables in mongoose models
 
