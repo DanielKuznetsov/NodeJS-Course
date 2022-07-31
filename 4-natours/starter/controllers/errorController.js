dd069586fb2536960b21d20d87b8a1e6e0870310
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-07-30 17:25:50 -0700

    created a user schema and model; provideda route to a post request for a user signup

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -60,7 +60,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
 
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -4,6 +4,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require(`./app.js`);
 
@@ -23,6 +29,14 @@ mongoose.connect(DB, { useUnifiedTopology: true }).then((connection) => {
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('App running on port ' + port);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
