6b042ab56fbe3c249284483f39bd9fcedfcfd9f6
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-08-05 21:11:13 -0700

    built an overview page with all of the tours presented on the page

--- 4-natours/starter/controllers/viewController.js
+++ 4-natours/starter/controllers/viewController.js
@@ -1,10 +1,20 @@
+const Tour = require(`.././models/tourModel`);
+const catchAsync = require('./../utilities/catchAsync');
 const express = require('express');
 
-exports.getOverview = function (req, res) {
+exports.getOverview = catchAsync(async function (req, res, next) {
+  // 1) Get all the tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render the template using the tour data from step 1
+
   res.status(200).render('overview', {
     title: 'All Tours', // ! this is a variable in pug file
+    tours,
   });
-};
+});
 
 exports.getTour = function (req, res) {
   res.status(200).render('tour', {
